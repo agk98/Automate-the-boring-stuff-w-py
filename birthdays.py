299e927b7008b8cb6c76b3d5cbe2552aaff58d8c
Ananth Kamath <agk98bbl@gmail.com>
2018-05-07 22:59:02 +0530

    adding some more files

---
+++ birthdays.py
@@ -0,0 +1,11 @@
+birthdays={'John':'May 12','Alice':'January 20','Hill':'March 21','Mark':'April 1'}
+print('Enter the Name whose birthday is to be found(Space to exit): ')
+name=input()
+if name==' ':
+        break
+if name in birthdays:
+    print(birthdays[name])
+else:
+    print('name does not exist')
+
+          
