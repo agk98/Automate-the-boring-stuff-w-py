801be0b36dcb1be8c0711a53b6c99ec64d75eb6d
Ananth Kamath <agk98bbl@gmail.com>
2018-05-09 22:40:21 +0530

    To know a person's birthday

--- birthdays.py
+++ birthdays.py
@@ -1,11 +1,13 @@
 birthdays={'John':'May 12','Alice':'January 20','Hill':'March 21','Mark':'April 1'}
-print('Enter the Name whose birthday is to be found(Space to exit): ')
-name=input()
-if name==' ':
-        break
-if name in birthdays:
-    print(birthdays[name])
-else:
-    print('name does not exist')
+while True:
+    print('Enter the Name whose birthday is to be found(Space to exit): ')
+
+    name=input()
+    if name==' ':
+            break
+    if name in birthdays:
+        print(birthdays[name])
+    else:
+        print('name does not exist')
 
           
