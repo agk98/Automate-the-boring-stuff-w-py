55320169a396c9ce4a0d762375883f5fd31d5d3a
Ananth Kamath <agk98bbl@gmail.com>
2018-05-20 11:55:22 +0530

    Adding

--- lucky.py
+++ lucky.py
@@ -1,16 +1,17 @@
-#! python 3
-#lucky.py-Opens several Google search results
+#! python3
+# lucky.py - Opens several Google search results.
 
+import requests, sys, webbrowser, bs4
 
-import bs4,requests, webbrowser, sys
-print('Googling...')#display text while downloading the google page
-res=requests.get('http://google.com/search?q='+' '.join(sys.argv[1:]))
+print('Googling...') # display text while downloading the Google page
+res = requests.get('http://google.com/search?q=' + ' '.join(sys.argv[1:]))
 res.raise_for_status()
-#TODO: Retrieve top search result links
-soup=bs4.BeautifulSoup(res.text)
 
-#TODO: Open a browser tab for each results
-linkElems=soup.select('.r a')
-numOpen=min(5, len(linkElems))
+# Retrieve top search result links.
+soup = bs4.BeautifulSoup(res.text)
+
+# Open a browser tab for each result.
+linkElems = soup.select('.r a')
+numOpen = min(5, len(linkElems))
 for i in range(numOpen):
-    webbrowser.open('http://google.com'+linkElems[i].get('href'))
+    webbrowser.open('http://google.com' + linkElems[i].get('href'))
